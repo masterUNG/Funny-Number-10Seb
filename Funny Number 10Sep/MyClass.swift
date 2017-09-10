924bd09ca898c24f69f28e8d295c38f1b66261b3
MasterUNG <phrombutr@gmail.com>
2017-09-10 11:51:38 +0700

    inc, dec OK

---
+++ Funny Number 10Sep/MyClass.swift
@@ -0,0 +1,18 @@
+//
+//  MyClass.swift
+//  Funny Number 10Sep
+//
+//  Created by MasterUNG on 9/10/2560 BE.
+//  Copyright © 2560 MasterUNG. All rights reserved.
+//
+
+import Foundation
+
+class MyClass{
+    
+    func decNumber(intNumber: Int) -> Int {
+        let result = intNumber - 1
+        return result
+    }
+    
+}
--- Funny Number 10Sep/ViewController.swift
+++ Funny Number 10Sep/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //Explicit
+    var numberAInt: Int = 1
+    
+    
     
     
     @IBOutlet weak var numberLabel: UILabel!
@@ -17,14 +21,27 @@ class ViewController: UIViewController {
     
     @IBAction func decButton(_ sender: Any) {
         print("Click DEC")
+        
+        let ungClass = MyClass()
+        numberAInt = ungClass.decNumber(intNumber: numberAInt)
+        numberLabel.text = String(numberAInt)
+        
+        
     }
       
     
     @IBAction func incButton(_ sender: Any) {
         print("Click INC")
+        numberAInt = addNumber(intNumber: numberAInt)
+        numberLabel.text = String(numberAInt)
+        
     }
     
-    
+    //Function Add Number
+    func addNumber(intNumber: Int) -> Int {
+        let result = intNumber + 1
+        return result
+    }
     
     
     
