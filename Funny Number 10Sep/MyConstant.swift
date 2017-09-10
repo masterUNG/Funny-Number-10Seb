2867f6a5e23982b050f5c943e31be2d2b09fed53
MasterUNG <phrombutr@gmail.com>
2017-09-10 15:39:38 +0700

    Success

---
+++ Funny Number 10Sep/MyConstant.swift
@@ -0,0 +1,22 @@
+//
+//  MyConstant.swift
+//  Funny Number 10Sep
+//
+//  Created by MasterUNG on 9/10/2560 BE.
+//  Copyright © 2560 MasterUNG. All rights reserved.
+//
+
+import Foundation
+
+class MyConstant {
+    
+    //Constant
+    let numberStrings = ["One", "Two", "Three", "Four", "Five", "Six", "Sevent", "Eight", "Nine", "Ten" ]
+    
+    func getterNumber() -> Array<Any> {
+        return numberStrings
+    }
+    
+    
+    
+}
--- Funny Number 10Sep/ShowOneViewController.swift
+++ Funny Number 10Sep/ShowOneViewController.swift
@@ -9,6 +9,58 @@
 import UIKit
 
 class ShowOneViewController: UIViewController {
+    
+    //Explicit
+    var objTimer = Timer()
+    var myConstant = MyConstant()
+    var indexAInt = 0
+    
+    
+    
+    
+        
+    
+    
+    
+    
+    
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    
+    @IBAction func playButton(_ sender: Any) {
+        print("Play")
+        objTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ShowOneViewController.myPlayTime), userInfo: nil, repeats: true)
+    }
+    
+    
+    @IBAction func stopButton(_ sender: Any) {
+        print("Stop")
+        objTimer.invalidate()
+    }
+    
+    func myPlayTime() -> Void {
+        
+        let nameStrings = myConstant.getterNumber()
+        
+        indexAInt += 1
+        if indexAInt >= nameStrings.count {
+            
+            indexAInt = 0
+        }
+        
+        showText(strName: nameStrings[indexAInt] as! String)
+        
+    }
+    
+    func showText(strName: String) -> Void {
+        
+        numberLabel.text = strName
+        
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
